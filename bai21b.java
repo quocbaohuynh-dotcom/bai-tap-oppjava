3340d1bfb2b7de01a021492842b266a27359b215
quocbaohuynh <baoax273@gmail.com>
2025-10-19 20:09:36 +0700

    bai tap

---
+++ bai21b.java
@@ -0,0 +1,62 @@
+public class bai21b {
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public bai21b(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = 0;
+    }
+
+    public bai21b(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String getAuthorEmail() {
+        return author.getEmail();
+    }
+
+    public char getAuthorGender() {
+        return author.getGender();
+    }
+
+    @Override
+    public String toString() {
+        return "Book[name=" + name + "," + bai21a.toString() +
+               ",price=" + price + ",qty=" + qty + "]";
+    } 
+}
